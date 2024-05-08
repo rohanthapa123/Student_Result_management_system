66c076ad21bffbcb88512bcd6db32748193704b9
Rohan Thapa <thaparohan2019@gmail.com>
2024-05-08 19:14:50 +0545

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,47 @@
 const express = require("express");
 const authcontroller = require("../controllers/authController");
-const { loggedIn, isAdmin, isTeacher, isStudent } = require("../middleware/auth.middleware");
-const routePermissionController = require('../controllers/routePermissionController')
+const {
+  loggedIn,
+  isAdmin,
+  isTeacher,
+  isStudent,
+} = require("../middleware/auth.middleware");
+const routePermissionController = require("../controllers/routePermissionController");
 const router = express.Router();
 
 router.post("/api/login", authcontroller.login);
 
-router.post("/api/logout",  authcontroller.logout);
+router.post("/api/logout", authcontroller.logout);
 
-router.post("/api/changepassword",loggedIn, authcontroller.changePassword)
-router.post("/api/resetpassword",loggedIn,isAdmin,authcontroller.resetPassword)
+router.post("/api/changepassword", loggedIn, authcontroller.changePassword);
+router.post(
+  "/api/resetpassword",
+  loggedIn,
+  isAdmin,
+  authcontroller.resetPassword
+);
 
-router.get("/api/check-auth",loggedIn,routePermissionController.isAuthenticated);
-router.get("/api/isAdmin",loggedIn,isAdmin, routePermissionController.isAuthenticated);
-router.get("/api/isTeacher",loggedIn,isTeacher,routePermissionController.isAuthenticated);
-router.get("/api/isStudent",loggedIn,isStudent,routePermissionController.isAuthenticated);
+router.get(
+  "/api/check-auth",
+  loggedIn,
+  routePermissionController.isAuthenticated
+);
+router.get(
+  "/api/isAdmin",
+  loggedIn,
+  isAdmin,
+  routePermissionController.isAuthenticated
+);
+router.get(
+  "/api/isTeacher",
+  loggedIn,
+  isTeacher,
+  routePermissionController.isAuthenticated
+);
+router.get(
+  "/api/isStudent",
+  loggedIn,
+  isStudent,
+  routePermissionController.isAuthenticated
+);
 module.exports = router;
